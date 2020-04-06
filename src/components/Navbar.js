f2914dd553fd537d543b99f0689ac0eede0767b4
Andrea <andreabarr87@gmail.com>
2020-04-06 15:25:28 -0500

    class to className, rendering in PatientPage, fixed patient fetch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,20 @@ const Navbar = () => {
 
 	return (
 		<div className="container">
-		<nav class="nav-extended teal lighten-3">
-			<div style={{paddingLeft:20}} class="nav-wrapper">
-			<a href="/" class="brand-logo">Vitalities</a>
-			{/* <a href="#"class="sidenav-trigger"><i class="material-icons">menu</i></a> */}
-			<ul id="nav-mobile" class="right hide-on-med-and-down">
+		<nav className="nav-extended teal lighten-3">
+			<div style={{paddingLeft:20}} className="nav-wrapper">
+			<a href="/" className="brand-logo">Vitalities</a>
+			{/* <a href="#"className="sidenav-trigger"><i className="material-icons">menu</i></a> */}
+			<ul id="nav-mobile" className="right hide-on-med-and-down">
 				<li><a href="/log-out">Log Out</a></li>
 			</ul>
 			</div>
-			<div class="nav-content">
-			<ul class="tabs tabs-transparent">
-				<li class="tab"><a class="active" href="/my-records">My Records</a></li>
-				<li class="tab disabled"><a href="/medication-manager">Medication Manager</a></li>
-				<li class="tab disabled"><a href="/insurance-cards">Insurance Cards</a></li>
-				<li class="tab disabled"><a href="/contacts">Contacts</a></li>
+			<div className="nav-content">
+			<ul className="tabs tabs-transparent">
+				<li className="tab"><a className="active" href="/my-records">My Records</a></li>
+				<li className="tab disabled"><a href="/medication-manager">Medication Manager</a></li>
+				<li className="tab disabled"><a href="/insurance-cards">Insurance Cards</a></li>
+				<li className="tab disabled"><a href="/contacts">Contacts</a></li>
 			</ul>
 			</div>
 		</nav>
--- src/components/PatientPage.js
+++ src/components/PatientPage.js
@@ -2,10 +2,26 @@ import React from 'react'
 
 class PatientPage extends React.Component {
 
+	//find appointments that match logged in patient's id
+	//filter by appointments in the future
+
+
+	renderAppointments = () => {
+		return this.props.patients.map(patient => {
+			if (patient.id === 12) {
+				return patient.first_name
+			}
+		})
+	}
+
 	render() {
 		return(
 			<div>
-			
+			<div className="row">
+				<div className="col s7">
+					{this.renderAppointments()}
+				</div>
+			</div>
 			</div>
 		)
 	}
